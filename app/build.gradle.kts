166a10b3abef170c58446f2a66fed2e628c69ced
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-26 21:15:25 +0800

    tea

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-
 }
 
 android {
@@ -30,19 +29,24 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,7 +55,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -67,6 +70,7 @@ dependencies {
     implementation(libs.mapbox.plugin.logo)
     implementation(libs.mapbox.plugin.scalebar)
     implementation(libs.mapbox.plugin.locationcomponent)
+    implementation(libs.dotenv.kotlin)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
